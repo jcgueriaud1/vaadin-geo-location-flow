2f3b597af813d40b5dc225683c27b8cde7e177f8
elmot <elmot@vaadin.com>
2018-05-17 11:57:36 +0300

    Improvements

--- src/test/java/org/vaadin/elmot/flow/sensors/DemoView.java
+++ src/test/java/org/vaadin/elmot/flow/sensors/DemoView.java
@@ -8,15 +8,23 @@ import com.vaadin.flow.theme.lumo.Lumo;
 @Route("")
 public class DemoView extends Div {
 
+    private static final int EARTH_RADIUS = 6371;
+
     public DemoView() {
         GeoLocation geoLocation = new GeoLocation();
         geoLocation.setWatch(true);
         geoLocation.setHighAccuracy(true);
         geoLocation.setTimeout(100000);
         geoLocation.setMaxAge(200000);
-        Label label = new Label();
-        geoLocation.addValueChangeListener( e->label.setText(e.getValue().toString()));
-        geoLocation.addErrorListener( e->label.setText(e.toString()));
-        add(geoLocation,label);
+        Div labelPos = new Div();
+        Div labelDist = new Div();
+        geoLocation.addValueChangeListener( e->{
+            Position position = e.getValue();
+            labelPos.setText(position.toString());
+            double dist = EARTH_RADIUS * Math.abs(position.getLatitude() * Math.PI / 180 );
+            labelDist.setText(String.format("Your distance from equator is %.2f km (%.2f nm)",dist, dist/1.852));
+        });
+        geoLocation.addErrorListener( e->labelPos.setText(e.toString()));
+        add(geoLocation,labelPos,labelDist);
     }
 }
