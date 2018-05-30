600b73c84ecb81211ffda469c0e66e1323e0a3e3
elmot <elmot@vaadin.com>
2018-05-30 12:11:06 +0300

    Improve demo + heroku config

--- src/test/java/org/vaadin/elmot/flow/sensors/DemoView.java
+++ src/test/java/org/vaadin/elmot/flow/sensors/DemoView.java
@@ -1,10 +1,11 @@
 package org.vaadin.elmot.flow.sensors;
 
 import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.html.Image;
 import com.vaadin.flow.component.html.Label;
 import com.vaadin.flow.router.Route;
-import com.vaadin.flow.theme.lumo.Lumo;
 
+@SuppressWarnings("unused")
 @Route("")
 public class DemoView extends Div {
 
@@ -18,13 +19,19 @@ public class DemoView extends Div {
         geoLocation.setMaxAge(200000);
         Div labelPos = new Div();
         Div labelDist = new Div();
-        geoLocation.addValueChangeListener( e->{
+        Image image = new Image();
+        geoLocation.addValueChangeListener(e -> {
             Position position = e.getValue();
             labelPos.setText(position.toString());
-            double dist = EARTH_RADIUS * Math.abs(position.getLatitude() * Math.PI / 180 );
-            labelDist.setText(String.format("Your distance from equator is %.2f km (%.2f nm)",dist, dist/1.852));
+            double dist = EARTH_RADIUS * Math.abs(position.getLatitude() * Math.PI / 180);
+            labelDist.setText(String.format("Your distance from equator is %.2f km (%.2f nm)", dist, dist / 1.852));
+            image.setSrc(String.format(
+                    "http://staticmap.openstreetmap.de/staticmap.php?center=%.2f,%.2f&zoom=10&size=865x512&maptype=mapnik"
+                    , position.getLatitude(), position.getLongitude()));
         });
-        geoLocation.addErrorListener( e->labelPos.setText(e.toString()));
-        add(geoLocation,labelPos,labelDist);
+        geoLocation.addErrorListener(e -> labelPos.setText(e.toString()));
+        add(geoLocation, labelPos, labelDist,
+                new Div(new Label("Somewhere here: ")),
+                image);
     }
 }
